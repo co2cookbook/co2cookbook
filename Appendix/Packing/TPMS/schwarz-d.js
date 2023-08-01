40e2257408df887b9835e7717bf1214c4bf055c8
Dan Grigsby <foo@bar.com>
2023-08-01 12:58:06 -0500

    vault backup: 2023-08-01 12:58:06

---
+++ Appendix/Packing/TPMS/schwarz-d.js
@@ -0,0 +1,66 @@
+{
+    "MathModels": [
+        {
+            "Iso3D": {
+                "Component": [
+                    "Schwarz D"
+                ],
+                "Fxyz": [
+                    "sin(x)*sin(y)*sin(z)*10 + sin(x)*cos(y)*cos(z)*10 + cos(x)*sin(y)*cos(z) + cos(x)*cos(y)*sin(z)"
+                ],
+                "Name": [
+                    "Schwarz"
+                ],
+                "Xmax": [
+                    " 4"
+                ],
+                "Xmin": [
+                    "-4"
+                ],
+                "Ymax": [
+                    " 4"
+                ],
+                "Ymin": [
+                    "-4"
+                ],
+                "Zmax": [
+                    " 4"
+                ],
+                "Zmin": [
+                    "-4"
+                ]
+            }
+        },
+        {
+            "Iso3D": {
+                "Component": [
+                    "Schwarz D"
+                ],
+                "Fxyz": [
+                    "sin(x)*sin(y)*sin(z) + sin(x)*cos(y)*cos(z) + cos(x)*sin(y)*cos(z) + cos(x)*cos(y)*sin(z)"
+                ],
+                "Name": [
+                    "Schwarz"
+                ],
+                "Xmax": [
+                    "10"
+                ],
+                "Xmin": [
+                    "0"
+                ],
+                "Ymax": [
+                    "10"
+                ],
+                "Ymin": [
+                    "0"
+                ],
+                "Zmax": [
+                    "10"
+                ],
+                "Zmin": [
+                    "0"
+                ]
+            }
+        }
+    ]
+}
